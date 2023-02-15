6ecad2ab8b9e5eca0dcdd890244acebe89e451e8
pazderskipawel <pawelpazderski@hotmail.com>
2023-02-15 15:01:39 +0100

    simplified login backend

--- login/views.py
+++ login/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import AuthenticationForm
-from .forms import NewUserForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
 def signup_view(request):
-    if request.method == "POST":
-        form = NewUserForm(request.POST)
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, "Registration successful.")
-        else:
-            messages.error(request, "Unsuccessful registration. Invalid information.")
-    form = NewUserForm()
-    return render(request, "login/signup.html", context={"register_form": form})
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'User '+ user +' successfully added')
+            redirect('login/signup')
+    context = {'register_form':form}
+    return render(request, "login/signup.html", context)
 
 
 def signin_view(request):
